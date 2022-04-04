dd80671bae37c31410031d8234babb9634abb61a
Présentation <thomas.broussard@epita.fr>
2022-04-04 16:33:51 +0200

    * titanic exercise

---
+++ titanic/src/fr/epita/titanic/datamodel/Passenger.java
@@ -0,0 +1,59 @@
+package fr.epita.titanic.datamodel;
+
+public class Passenger {
+
+
+    private String passengerId;
+    private boolean survived;
+    private String pclass;
+    private String gender;
+    private double age;
+
+    public String getPassengerId() {
+        return passengerId;
+    }
+
+    public void setPassengerId(String passengerId) {
+        this.passengerId = passengerId;
+    }
+
+    public boolean getSurvived() {
+        return survived;
+    }
+
+    public void setSurvived(boolean survived) {
+        this.survived = survived;
+    }
+
+    public String getPclass() {
+        return pclass;
+    }
+
+    public void setPclass(String pclass) {
+        this.pclass = pclass;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Passenger(String passengerId, boolean survived, String pclass, String gender, double age) {
+        this.passengerId = passengerId;
+        this.survived = survived;
+        this.pclass = pclass;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public void setAge(double age) {
+        this.age = age;
+    }
+}
--- titanic/src/fr/epita/titanic/launchers/Launcher.java
+++ titanic/src/fr/epita/titanic/launchers/Launcher.java
@@ -1,9 +1,9 @@
 package fr.epita.titanic.launchers;
 
+import fr.epita.titanic.datamodel.Passenger;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Launcher {
 
@@ -13,17 +13,32 @@ public class Launcher {
             System.out.println("the file has been found!");
         }
         Scanner scanner = new Scanner(file);
+        List<Passenger> passengers = new ArrayList<>();
+        double averageAge = 0;
         while (scanner.hasNext()){
             String line = scanner.nextLine();
             String[] split = line.split(",");
 
             String passengerId = split[0];
-            String survived = split[1];
+            boolean survived = Boolean.valueOf(split[1]);
             String pclass = split[2];
             String gender = split[3];
+            double age = Double.valueOf(split[4]);
 
+            Passenger passenger = new Passenger(passengerId,survived, pclass,gender, age);
+            passengers.add(passenger);
+            averageAge += passenger.getAge();
 
         }
+        averageAge = averageAge / passengers.size();
+
+        double average = passengers
+                .stream()
+                .mapToDouble(Passenger::getAge)
+                .average()
+                .getAsDouble();
+
+        System.out.println("average: " + average);
 
     }
 }
